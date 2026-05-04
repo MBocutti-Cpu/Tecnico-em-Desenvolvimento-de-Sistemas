d1c3b02104087c332a5fd0d151c7d02250380b2d
Matheus Bocutti Colaço <matheusbocuttie@gmail.com>
2026-05-04 19:53:54 -0300

    ookay

---
+++ aula3.py
@@ -0,0 +1,9 @@
+sinal = input("Digite o sinal:")
+if sinal== "vermelho":
+    print("PARE!")
+elif sinal == "amarelo":
+    print("ATENÇÃO")
+elif sinal == "verde":
+    print("pode passar")
+else:
+    print("não existe sinal")
