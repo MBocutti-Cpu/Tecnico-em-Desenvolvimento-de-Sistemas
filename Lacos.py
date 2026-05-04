2b661b2a97354f4f7e1a0d7b3bc4852b1ef36d98
Matheus Bocutti Colaço <matheusbocuttie@gmail.com>
2026-05-04 19:48:56 -0300

    colado

--- Lacos.py
+++ Lacos.py
@@ -1,2 +1,10 @@
-dcjkjscijcjsprint ("esssa fera ai meu") 
-input ("boa noite meus caros")
+idade = int(input("Idade:")) 
+if idade < 13:
+    print ("criança")
+elif idade < 18:
+    print("adolescente")
+elif idade < 65:
+    print("adulto")
+else:
+    print("idoso")
+    
